6cc56f2f54f6c35002a790d719fc5ff475e6340c
Mehdisaber332 <mehdisaber332@gmail.com>
2023-08-14 08:59:52 -0700

    starting

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "shell.h"
+
+int main(void)
+{
+	simple_shell();
+	return (0);
+}
---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+#define MAX_INPUT_SIZE 1024
+
+void simple_shell(void)
+{
+	while (1)
+    {
+	printf("($) ");
+	char input[MAX_INPUT_SIZE];
+	if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+		perror("fgets");
+	break;
+        }
+	input[strcspn(input, "\n")] = '\0';
+	}
+}
