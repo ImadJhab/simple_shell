7aa8607d48540b896a56545411268ac69479e3c4
Mehdisaber332 <mehdisaber332@gmail.com>
2023-08-25 05:02:18 -0700

    Lets fix that

--- custom_getline.c
+++ custom_getline.c
@@ -3,15 +3,16 @@
 #define CUSTOM_READ_SIZE 1024
 
 /**
- * custom_getline - gets a line from a file descriptor
- * @ln: pointer to store the line
- * @bufsize: size of the buffer
- * @fd: file descriptor
- * Return: number of characters read
+ * readLine - Reads a line from a file descriptor.
+ * @ln: Pointer to store the line.
+ * @bufsize: Size of the buffer.
+ * @fd: File descriptor.
+ * Return: Number of characters read.
  */
-ssize_t custom_getline(char **ln, size_t *bufsize, int fd)
+ssize_t readLine(char **ln, size_t *bufsize, int fd)
+
 {
-	ssize_t num_chars = 0;
+	ssize_t numChars = 0;
 
 	if (ln == NULL || bufsize == NULL)
 		return (-1);
@@ -24,12 +25,42 @@ ssize_t custom_getline(char **ln, size_t *bufsize, int fd)
 		*bufsize = CUSTOM_READ_SIZE;
 	}
 
-	num_chars = read(fd, *ln, *bufsize);
-	if (num_chars == -1)
+	numChars = read(fd, *ln, *bufsize);
+	if (numChars == -1)
 	{
 		free(*ln);
 		return (-1);
 	}
 
-	return (num_chars);
+	return (numChars);
+}
+
+/**
+ * initializeBuffer - Initializes the line buffer.
+ * @ln: Pointer to store the line.
+ * @bufsize: Size of the buffer.
+ */
+void initializeBuffer(char **ln, size_t *bufsize)
+
+{
+	if (ln != NULL && *ln == NULL && bufsize != NULL)
+	{
+		*ln = malloc(sizeof(char) * CUSTOM_READ_SIZE);
+		if (*ln != NULL)
+			*bufsize = CUSTOM_READ_SIZE;
+	}
+}
+
+/**
+ * freeBuffer - Frees the line buffer.
+ * @ln: Pointer to the line buffer.
+ */
+void freeBuffer(char **ln)
+
+{
+	if (ln != NULL && *ln != NULL)
+	{
+		free(*ln);
+		*ln = NULL;
+	}
 }
